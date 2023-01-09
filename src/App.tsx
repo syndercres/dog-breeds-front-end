6d038ea1249d8a0af55ee5dadb5063b8df6e6ea4
Alessia B <borys.alessia@gmail.com>
2023-01-09 17:04:01 +0100

    fetching from API & helper function for breedNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { greet } from "./utils/greet";
+import VotingPage from "./components/VotingPage";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+    <h1>{greet("World")}</h1>
+    <VotingPage />
+    </>)
 }
 
 export default App;
