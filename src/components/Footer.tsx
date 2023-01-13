832db3e34ea3631e7c0a0f7d5cf76f368e3a9efe
Alessia B <borys.alessia@gmail.com>
2023-01-13 14:34:39 +0100

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,48 @@
+import linkedinIcon from "../images/linkedinIcon.png";
+
+export default function Footer(): JSX.Element {
+  return (
+    <>
+      <p>
+        API: <a href="https://dog.ceo/">DogCEO</a>
+      </p>
+      <div className="linkedin-container">
+        <a
+          className="linkedin-btn"
+          href="https://www.linkedin.com/in/alessiaborys/"
+        >
+          <img
+            src={linkedinIcon}
+            alt="linkedin-icon"
+            className="linkedin-icon"
+          />
+          <span className="linkedin-name">Alessia</span>
+        </a>
+        <a className="linkedin-btn" href="https://www.linkedin.com/in/XX/">
+          <img
+            src={linkedinIcon}
+            alt="linkedin-icon"
+            className="linkedin-icon"
+          />
+          <span className="linkedin-name">Grace</span>
+        </a>
+        <a className="linkedin-btn" href="https://www.linkedin.com/in/XX/">
+          <img
+            src={linkedinIcon}
+            alt="linkedin-icon"
+            className="linkedin-icon"
+          />
+          <span className="linkedin-name">Maghfoor</span>
+        </a>
+        <a className="linkedin-btn" href="https://www.linkedin.com/in/XX/">
+          <img
+            src={linkedinIcon}
+            alt="linkedin-icon"
+            className="linkedin-icon"
+          />
+          <span className="linkedin-name">Sinbad</span>
+        </a>
+      </div>
+    </>
+  );
+}
